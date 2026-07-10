9a64637a850bea100cbf3469780e31765acb9344
Lakshya <lakshyakant007@gmail.com>
2026-07-10 11:19:57 +0530

    Second

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://sentiment-api.kindpond-fa4065cb.centralindia.azurecontainerapps.io",
         changeOrigin: true,
       },
     },
